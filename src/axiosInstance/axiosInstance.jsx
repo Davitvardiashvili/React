db0a39f6b4a243dd03d01e5fd7f3c07cabd75451
Davitvardiashvili <dato.vardiashvili2001@gmail.com>
2024-04-10 23:06:03 +0400

    mobile responsive

--- src/axiosInstance/axiosInstance.jsx
+++ src/axiosInstance/axiosInstance.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.1.5:8000/api/'
+  // baseURL: 'http://192.168.1.5:8000/api/'
   // baseURL: 'http://localhost:8000/api/'
- // baseURL: 'https://api.results.gsf.ge/api/'
+  baseURL: 'https://api.results.gsf.ge/api/'
 });
 
 axiosInstance.interceptors.request.use(
